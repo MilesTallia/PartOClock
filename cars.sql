f5b6783d97b68eba57cadead90d400cc36f35a0c
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-02-19 11:24:22 -0500

    connections and connections!

--- cars.sql
+++ cars.sql
@@ -1,13 +1,26 @@
+-- STRUCTURE
 DROP TABLE IF EXISTS parts;
 DROP TABLE IF EXISTS assemblies;
 DROP TABLE IF EXISTS subsystems;
 DROP TABLE IF EXISTS cars;
-
+-- CONNECTIONS                                      -- sooooooo many of these - one for each componenet -> assembly / componenet -> subsytem connection
+DROP TABLE IF EXISTS subsystemToCar;
+DROP TABLE IF EXISTS assemblyToSubsystem;
+DROP TABLE IF EXISTS assemblyToAssembly;
+DROP TABLE IF EXISTS partToSubsystem;
+DROP TABLE IF EXISTS partToAssembly;
+-- TIME
 DROP TABLE IF EXISTS runtimeTypes;
 DROP TABLE IF EXISTS runtimes;
+-- TIME CONNECTIONS                                 -- also a lot of these, one for each time the car is run and for each individual part
+DROP TABLE IF EXISTS timeToPart;
+DROP TABLE IF EXISTS timeToAssembly;
+DROP TABLE IF EXISTS timeToCar;
 
-DROP TABLE IF EXISTS childToParent;      -- sooooooo many of these - one for each part -> assembly / assembly->subsytem connection
-DROP TABLE IF EXISTS timeToComponent;     -- also a lot of these, one for each time the car is run and for each individual component
+
+
+
+-- STRUCTURE
 
 CREATE TABLE parts(
     ID SERIAL PRIMARY KEY,
@@ -32,6 +45,46 @@ CREATE TABLE cars(
     identifyer TEXT NOT NULL
 );
 
+
+
+
+-- CONNECTIONS
+
+CREATE TABLE subsystemToCar(
+    ID SERIAL PRIMARY KEY,
+    childID INTEGER NOT NULL,
+    parentID INTEGER NOT NULL
+);
+
+CREATE TABLE assemblyToSubsystem(
+    ID SERIAL PRIMARY KEY,
+    childID INTEGER NOT NULL,
+    parentID INTEGER NOT NULL
+);
+
+CREATE TABLE assemblyToAssembly(
+    ID SERIAL PRIMARY KEY,
+    childID INTEGER NOT NULL,
+    parentID INTEGER NOT NULL
+);
+
+CREATE TABLE partToSubsystem(
+    ID SERIAL PRIMARY KEY,
+    childID INTEGER NOT NULL,
+    parentID INTEGER NOT NULL
+);
+
+CREATE TABLE partToAssembly(
+    ID SERIAL PRIMARY KEY,
+    childID INTEGER NOT NULL,
+    parentID INTEGER NOT NULL
+);
+
+
+
+
+-- TIME
+
 CREATE TABLE runtimeTypes(
     ID SERIAL PRIMARY KEY,
     runtimeType TEXT NOT NULL
@@ -44,17 +97,19 @@ CREATE TABLE runtimes(
     dateRan TIMESTAMP NOT NULL
 );
 
-CREATE TABLE childToParent(
+
+
+
+-- TIME CONNECTIONS
+
+CREATE TABLE runtimeToPart(
     ID SERIAL PRIMARY KEY,
     childID INTEGER NOT NULL,
-    parentID INTEGER NOT NULL,
-    childType TEXT NOT NULL,
-    parentType TEXT NOT NULL
+    parentID INTEGER NOT NULL
 );
 
-CREATE TABLE timeToComponent(
+CREATE TABLE runtimeToCar(
     ID SERIAL PRIMARY KEY,
-    timeID INTEGER NOT NULL,
-    componentID INTEGER NOT NULL,
-    componentType TEXT NOT NULL
+    childID INTEGER NOT NULL,
+    parentID INTEGER NOT NULL
 );

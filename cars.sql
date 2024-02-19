cf0e2d02f20b82275cd373d7f7c0cb290c5e644f
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-02-19 11:25:30 -0500

    minor changes

--- cars.sql
+++ cars.sql
@@ -3,18 +3,20 @@ DROP TABLE IF EXISTS parts;
 DROP TABLE IF EXISTS assemblies;
 DROP TABLE IF EXISTS subsystems;
 DROP TABLE IF EXISTS cars;
+
 -- CONNECTIONS                                      -- sooooooo many of these - one for each componenet -> assembly / componenet -> subsytem connection
 DROP TABLE IF EXISTS subsystemToCar;
 DROP TABLE IF EXISTS assemblyToSubsystem;
 DROP TABLE IF EXISTS assemblyToAssembly;
 DROP TABLE IF EXISTS partToSubsystem;
 DROP TABLE IF EXISTS partToAssembly;
+
 -- TIME
 DROP TABLE IF EXISTS runtimeTypes;
 DROP TABLE IF EXISTS runtimes;
+
 -- TIME CONNECTIONS                                 -- also a lot of these, one for each time the car is run and for each individual part
 DROP TABLE IF EXISTS timeToPart;
-DROP TABLE IF EXISTS timeToAssembly;
 DROP TABLE IF EXISTS timeToCar;
 
 
